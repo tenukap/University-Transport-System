9371decbc9ac8f6eb6b51d7380ab8c388040f81f
Tenuka <tenukaperera1025@gmail.com>
2026-08-11 22:02:26 +0530

    first commit and skeleton

---
+++ src/main/java/com/transport/uni_transport_system/UniTransportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.transport.uni_transport_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTransportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTransportSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transport/uni_transport_system/UniTransportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transport.uni_transport_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniTransportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
